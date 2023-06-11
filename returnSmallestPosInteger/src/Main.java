043293250138c0cf2d6801d5f3713960d5bf250a
aqil <hello@aqil.com>
2023-06-11 22:01:27 +0800

    find min num

--- returnSmallestPosInteger/src/Main.java
+++ returnSmallestPosInteger/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        int[] A = {1,2,3,4,7,9};
+        int arraySize;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements to store: ");
+        arraySize = sc.nextInt();
+        int[] arrayMemory = new int[10];
+
+        System.out.println("Enter the elements of the array: ");
+
+        for(int i = 0; i < arraySize; i++)
+        {
+            arrayMemory[i] = sc.nextInt();
+        }
+
+        int[] A = arrayMemory;
 
         Solution minNum = new Solution();
         System.out.println(minNum.solution(A));
