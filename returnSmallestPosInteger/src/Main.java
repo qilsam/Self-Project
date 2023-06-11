fc864aa054bd8f0d53c741caa027c1f00a0bd482
aqil <hello@aqil.com>
2023-06-11 23:16:09 +0800

    find min num

--- returnSmallestPosInteger/src/Main.java
+++ returnSmallestPosInteger/src/Main.java
@@ -7,15 +7,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter how many numbers to store (Max 10): ");
+
         arraySize = sc.nextInt();
+
         int[] A = new int[10];
 
         System.out.println("Enter the numbers: ");
 
-        for(int i = 0; i < arraySize; i++)
+        try
         {
-            A[i] = sc.nextInt();
+            for (int i = 0; i < arraySize; i++)
+            {
+                A[i] = sc.nextInt();
+            }
         }
+        catch (Exception e)
+        {
+            System.out.println("Please enter a number.");
+        }
+
 
         Solution minNum = new Solution();
         System.out.println("Smallest positive integer that is missing is: " + minNum.solution(A));
